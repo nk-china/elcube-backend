b20ea271e6e8780d6ed2bfede7cb6b228af67a6d
bean <mailxbs@126.com>
2021-07-27 22:23:37 +0800

    ....

---
+++ ts5-platform-temp/src/main/java/cn/nkpro/ts5/cards/header/NkCardHeaderDefault.groovy
@@ -0,0 +1,21 @@
+package cn.nkpro.ts5.cards.header
+
+import cn.nkpro.ts5.basic.wsdoc.annotation.WsDocNote
+import cn.nkpro.ts5.engine.doc.NKAbstractCard
+import org.springframework.stereotype.Component
+
+
+@WsDocNote("交易抬头")
+@Component("NkCardHeaderDefault")
+class NkCardHeaderDefault extends NKAbstractCard<Map,Map> {
+
+    @Override
+    String getPosition() {
+        return POSITION_HEADER
+    }
+
+    @Override
+    String[] getDefComponentNames() {
+        return {"NkCardHeaderDefaultDef"}
+    }
+}
