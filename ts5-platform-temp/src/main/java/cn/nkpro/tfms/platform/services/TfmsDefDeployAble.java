b526005a40ea27f071e05054c9291004a1161b0b
bean <mailxbs@126.com>
2021-12-02 11:27:22 +0800

    完善单据权限，以及单据运行时的处理

--- ts5-platform-temp/src/main/java/cn/nkpro/tfms/platform/services/TfmsDefDeployAble.java
+++
@@ -1,9 +0,0 @@
-package cn.nkpro.tfms.platform.services;
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface TfmsDefDeployAble {
-    int deployOrder();
-    Object deployExport(JSONObject config);
-    void deployImport(Object data);
-}
--- ts5-platform-temp/src/main/java/cn/nkpro/tfms/platform/services/TfmsStreamTask.java
+++
@@ -1,8 +0,0 @@
-package cn.nkpro.tfms.platform.services;
-
-import cn.nkpro.tfms.platform.basis.TfmsCustomObject;
-
-public interface TfmsStreamTask extends TfmsCustomObject {
-
-    void onMessage(TfmsStreamTaskInfo info);
-}
