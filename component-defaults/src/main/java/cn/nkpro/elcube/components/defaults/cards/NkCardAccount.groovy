4ecaff6d7d55cfaba730ad17ec9f9cd097ced1f4
bean <mailxbs@126.com>
2021-12-17 19:07:28 +0800

    增加关联用户账号卡片

---
+++ component-defaults/src/main/java/cn/nkpro/elcube/components/defaults/cards/NkCardAccount.groovy
@@ -0,0 +1,63 @@
+package cn.nkpro.elcube.components.defaults.cards
+
+import cn.nkpro.elcube.annotation.NkNote
+import cn.nkpro.elcube.docengine.NkAbstractCard
+import cn.nkpro.elcube.docengine.model.DocDefIV
+import cn.nkpro.elcube.docengine.model.DocHV
+import cn.nkpro.elcube.exception.NkOperateNotAllowedCaution
+import cn.nkpro.elcube.security.SecurityUtilz
+import cn.nkpro.elcube.security.UserAccountService
+import org.apache.commons.lang3.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.util.Assert
+
+@NkNote("账号绑定")
+@Component("NkCardAccount")
+class NkCardAccount extends NkAbstractCard<Data,Map> {
+
+    @Autowired
+    private UserAccountService userAccountService
+
+    @Override
+    Data beforeUpdate(DocHV doc, Data data, Data original, DocDefIV defIV, Map d) {
+
+        if(SecurityUtilz.hasAnyAuthority('*:*','SETTINGS:*','SETTINGS:AUTH')){
+
+            if(data.account != original.account){
+
+                if(original.account){
+                    def account = userAccountService.getAccount(original.account, false)
+                    account.setObjectId(StringUtils.EMPTY)
+                    account.setPassword(null)
+                    userAccountService.update(account)
+                }
+
+                if(data.account){
+                    def account = userAccountService.getAccount(data.account, false)
+
+                    Assert.notNull(account, "账号不存在")
+
+                    if(account.getObjectId()!=doc.getDocId()){
+
+                        if(account.getObjectId()){
+                            throw new NkOperateNotAllowedCaution("账单已被其他交易伙伴绑定")
+                        }else{
+                            account.setObjectId(doc.getDocId())
+                            account.setPassword(null)
+                            userAccountService.update(account)
+                        }
+                    }
+                }
+            }
+        }else{
+            data.account = original.account
+        }
+
+        return super.beforeUpdate(doc, data, original, defIV, d) as Data
+    }
+
+    static class Data{
+        String account
+    }
+}
---
+++ component-defaults/src/main/java/cn/nkpro/elcube/components/defaults/cards/NkCardAccount.vue
@@ -0,0 +1,30 @@
+<!--
+	This file is part of ELCube.
+	ELCube is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	ELCube is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	You should have received a copy of the GNU Affero General Public License
+	along with ELCube.  If not, see <https://www.gnu.org/licenses/>.
+-->
+<template>
+    <nk-card>
+        <nk-form :edit="editMode" :col="2">
+            <nk-form-item title="关联用户账号">
+                <router-link v-if="data.account" :to="`/apps/settings/auth/accounts/detail/${data.account}`">{{data.account}}</router-link>
+                <a-input v-model="data.account" slot="edit"></a-input>
+            </nk-form-item>
+        </nk-form>
+    </nk-card>
+</template>
+
+<script>
+    import Mixin from "Mixin";
+    export default {
+        mixins:[Mixin()],
+    }
+</script>
