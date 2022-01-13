7a4be6156263ec9823545837e882f5896e1dedb2
bean <mailxbs@126.com>
2022-01-13 13:42:32 +0800

    修复抬头卡片不计算的bug

--- component-defaults/src/main/java/cn/nkpro/elcube/components/defaults/cards/header/NkCardHeaderDefault.groovy
+++ component-defaults/src/main/java/cn/nkpro/elcube/components/defaults/cards/header/NkCardHeaderDefault.groovy
@@ -68,6 +68,22 @@ class NkCardHeaderDefault extends NkAbstractCard<Map,Def> {
     }
 
     @Override
+    Map calculate(DocHV doc, Map data, DocDefIV defIV, Def d, boolean isTrigger, Object options) {
+
+        def context = spELManager.createContext(doc)
+
+        if(StringUtils.isNotBlank(d.getPartnerIdSpEL())){
+            doc.setPartnerId(spELManager.invoke(d.getPartnerIdSpEL(), context) as String)
+        }
+
+        if(StringUtils.isNotBlank(d.getPartnerNameSpEL())){
+            doc.setPartnerName(spELManager.invoke(d.getPartnerNameSpEL(), context) as String)
+        }
+
+        return super.calculate(doc, data, defIV, d, isTrigger, options) as Map
+    }
+
+    @Override
     Map beforeUpdate(DocHV doc, Map data, Map original, DocDefIV defIV, Def d) {
 
         def context = spELManager.createContext(doc)
