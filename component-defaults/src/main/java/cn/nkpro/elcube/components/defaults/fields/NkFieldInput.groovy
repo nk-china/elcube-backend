103ece915264f5e5134ae073ee2b341d070be234
Wang Feng <wangfeng@newcore.net.cn>
2022-01-24 10:13:16 +0800

    文本框add 执行SpEL表达式 数据映射模版

--- component-defaults/src/main/java/cn/nkpro/elcube/components/defaults/fields/NkFieldInput.groovy
+++ component-defaults/src/main/java/cn/nkpro/elcube/components/defaults/fields/NkFieldInput.groovy
@@ -14,18 +14,45 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with ELCube.  If not, see <https://www.gnu.org/licenses/>.
  */
-package cn.nkpro.elcube.components.defaults.fields;
+package cn.nkpro.elcube.components.defaults.fields
 
-import cn.nkpro.elcube.annotation.NkNote;
+import cn.nkpro.elcube.annotation.NkNote
+import cn.nkpro.elcube.co.easy.EasySingle
+import cn.nkpro.elcube.co.spel.NkSpELManager
 import cn.nkpro.elcube.docengine.NkAbstractField
+import cn.nkpro.elcube.docengine.cards.NkCalculateContext
+import cn.nkpro.elcube.docengine.cards.NkDynamicFormDefI
 import cn.nkpro.elcube.docengine.cards.NkDynamicFormField
 import cn.nkpro.elcube.docengine.cards.NkDynamicGridField
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import com.alibaba.fastjson.JSON
+import org.apache.commons.lang3.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.annotation.Order
+import org.springframework.expression.EvaluationContext
+import org.springframework.stereotype.Component
 
 @Order(10)
 @NkNote("文本")
 @Component("NkFieldInput")
 class NkFieldInput extends NkAbstractField implements NkDynamicFormField, NkDynamicGridField {
+    @Autowired
+    private NkSpELManager spELManager
 
+    @Override
+    void beforeCalculate(NkDynamicFormDefI field, EvaluationContext context, EasySingle card, NkCalculateContext calculateContext) {
+        if (calculateContext.isFieldTrigger() || !Objects.equals(card.get(field.getKey()), calculateContext.original.get(field.getKey()))) {
+            String dataMappings = field.getInputOptions().get("dataMappings")
+            String method = field.getInputOptions().get("method")
+
+            if(StringUtils.isNotBlank(method)) {
+                def jSONObject = JSON.toJSON(spELManager.invoke(method, context))
+                if (StringUtils.isNotBlank(dataMappings)) {
+                    def result = JSON.parseObject(spELManager.convert(dataMappings, jSONObject))
+                    result.forEach({ k, v ->
+                        card.set(k, v)
+                    })
+                }
+            }
+        }
+    }
 }
