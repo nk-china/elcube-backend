2090641ec3c5b1da88b134e467532d427b7f2d22
liuyingze <liuyingze@newcore.net.cn>
2022-01-25 17:13:00 +0800

    恢复文件

---
+++ demo/src/main/java/cn/nkpro/elcube/components/cr/pojo/WeightCategory.java
@@ -0,0 +1,28 @@
+package cn.nkpro.elcube.components.cr.pojo;
+
+public class WeightCategory{
+
+    private String category;//类别
+    private int weight;//权重值
+
+    public WeightCategory(String category, int weight) {
+        this.category = category;
+        this.weight = weight;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
