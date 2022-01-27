1d5039eeaa68dd1940acfe8ae715c5ee7c2160ff
bean <mailxbs@126.com>
2022-01-27 21:19:37 +0800

    增加EQL语言，来更新单据

--- docengine/src/main/java/cn/nkpro/elcube/docengine/NkEqlEngine.java
+++ docengine/src/main/java/cn/nkpro/elcube/docengine/NkEqlEngine.java
@@ -10,6 +10,10 @@ import java.util.List;
 
 /**
  *
+ * ELCube Query Language
+ *
+ * 注意：包含特殊字符的复杂的SpEL，需要用 " 或 ` 符号包裹
+ *
  * SELECT * |
  *         <SpEL>  [as alias],
  *        `<SpEL>` [as alias],
@@ -51,11 +55,34 @@ import java.util.List;
  *    AND dynamic.<indexKey> IS NULL
  *    AND dynamic.<indexKey> IS NOT NULL
  *
+ *    // d 和 dy 是 dynamic 的别名，作用是一样的
  *    AND d.<indexKey> [=|>|>=|<|<=|!=|<>|LIKE] <value>
  *    AND d.<indexKey> BETWEEN <value1> AND <value2>
  *    AND d.<indexKey> IN (...<value>)
  *    AND d.<indexKey> IS NULL
  *    AND d.<indexKey> IS NOT NULL
+ *
+ * etc.
+ *
+ *  select docName,
+ *         data.customer.name,
+ *         "data.customer.name" as name1,
+ *         `data.customer.name` as name2,
+ *         'data.customer.name' as name3,
+ *         `data.plans?.^[true]?.name` as plan_name,
+ *         //el('data.plans?.^[true]?.name') // 这种函数语法不支持，可使用SpEL来执行函数
+ *    from doc
+ *   where d.abc is null
+ *     and docId = '11bd25ac-c303-484b-b7c0-c66816e18cf2'
+ *
+ *  update doc
+ *     set docName = '123',
+ *         "data.payment.^[true].remark" = '123',
+ *         createdTime = 0,
+ *         `partnerName` = null,
+ *         docName = `data.payment.^[true].expireDate`
+ *   where docId = '13b24c8a-66aa-4753-8444-b790d8602c7f'
+ *
  */
 public interface NkEqlEngine {
 
