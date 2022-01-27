7b879eefb131a8cbee3f691eeef0992d4c1d2968
bean <mailxbs@126.com>
2022-01-27 21:13:15 +0800

    增加EQL语言，来更新单据

--- docengine/src/main/java/cn/nkpro/elcube/docengine/NkEqlEngine.java
+++ docengine/src/main/java/cn/nkpro/elcube/docengine/NkEqlEngine.java
@@ -8,11 +8,65 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ *
+ * SELECT * |
+ *         <SpEL>  [as alias],
+ *        `<SpEL>` [as alias],
+ *        "<SpEL>" [as alias]
+ *   FROM [doc|all|<docType>]
+ *  WHERE <property> [=|>|>=|<|<=|!=|<>|LIKE] <value>
+ *    AND <property> BETWEEN <value1> AND <value2>
+ *    AND <property> IN (...<value>)
+ *    AND <property> IS NULL
+ *    AND <property> IS NOT NULL
+ *
+ *    AND dynamic.<indexKey> [=|>|>=|<|<=|!=|<>|LIKE] <value>
+ *    AND dynamic.<indexKey> BETWEEN <value1> AND <value2>
+ *    AND dynamic.<indexKey> IN (...<value>)
+ *    AND dynamic.<indexKey> IS NULL
+ *    AND dynamic.<indexKey> IS NOT NULL
+ *
+ *    AND d.<indexKey> [=|>|>=|<|<=|!=|<>|LIKE] <value>
+ *    AND d.<indexKey> BETWEEN <value1> AND <value2>
+ *    AND d.<indexKey> IN (...<value>)
+ *    AND d.<indexKey> IS NULL
+ *    AND d.<indexKey> IS NOT NULL
+ *
+ *
+ * UPDATE [doc|all|<docType>]
+ *    SET  <property> = <property> | `<SpEL>` | "<SpEL>" | <Number> | '<String>',
+ *         <SpEL>     = <property> | `<SpEL>` | "<SpEL>" | <Number> | '<String>',
+ *        `<SpEL>`    = <property> | `<SpEL>` | "<SpEL>" | <Number> | '<String>',
+ *        "<SpEL>"    = <property> | `<SpEL>` | "<SpEL>" | <Number> | '<String>'
+ *  WHERE <property> [=|>|>=|<|<=|!=|<>|LIKE] <value>
+ *    AND <property> BETWEEN <value1> AND <value2>
+ *    AND <property> IN (...<value>)
+ *    AND <property> IS NULL
+ *    AND <property> IS NOT NULL
+ *
+ *    AND dynamic.<indexKey> [=|>|>=|<|<=|!=|<>|LIKE] <value>
+ *    AND dynamic.<indexKey> BETWEEN <value1> AND <value2>
+ *    AND dynamic.<indexKey> IN (...<value>)
+ *    AND dynamic.<indexKey> IS NULL
+ *    AND dynamic.<indexKey> IS NOT NULL
+ *
+ *    AND d.<indexKey> [=|>|>=|<|<=|!=|<>|LIKE] <value>
+ *    AND d.<indexKey> BETWEEN <value1> AND <value2>
+ *    AND d.<indexKey> IN (...<value>)
+ *    AND d.<indexKey> IS NULL
+ *    AND d.<indexKey> IS NOT NULL
+ */
 public interface NkEqlEngine {
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     List<? extends DocH> executeEql(String eql);
 
+    /**
+     *
+     * @param eql ELCube Query Language
+     * @return List
+     */
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     List<DocHQL> findByEql(String eql);
 
