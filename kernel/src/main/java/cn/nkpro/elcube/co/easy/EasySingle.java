af20f2eac0491e4a96b2f2c5771cdbc11b18bb88
libowen <504088765@qq.com>
2022-02-28 12:48:32 +0800

    增加克隆机制

--- kernel/src/main/java/cn/nkpro/elcube/co/easy/EasySingle.java
+++ kernel/src/main/java/cn/nkpro/elcube/co/easy/EasySingle.java
@@ -22,7 +22,7 @@ import java.util.Collection;
 import java.util.Map;
 
 @SuppressWarnings("all")
-public interface EasySingle {
+public interface EasySingle extends Cloneable{
 
     Object target();
 
